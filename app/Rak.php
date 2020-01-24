8370e096d9417bc3cb459b1bd7acc517f8e36d68
YogaHadiN <yoga_email@yahoo.com>
2020-01-24 10:50:24 +0700

    this2

--- app/Rak.php
+++ app/Rak.php
@@ -65,9 +65,7 @@ class Rak extends Model{
 	protected $guarded = [];
 
 	public function formula(){
-
-
-		return $this->belongsTo('App\Formula');
+		return $this->belongsTo('Formula');
 	}
 
 	public function merek(){
