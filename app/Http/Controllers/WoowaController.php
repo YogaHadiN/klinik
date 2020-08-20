a4830863970175577da38d86b0ee5203fc79ff14
YogaHadiN <yoga_email@yahoo.com>
2020-08-20 14:10:20 +0700

    testwebhook

--- app/Http/Controllers/WoowaController.php
+++ app/Http/Controllers/WoowaController.php
@@ -9,6 +9,7 @@ use Log;
 class WoowaController extends Controller
 {
 	public function webhook(){
+		Log::info('test webhook');
 		$json = file_get_contents('php://input');
 		$data = json_decode($json);
 		file_put_contents("listen.txt", print_r($data,1));
