1192c2bb9520338428e9de689df87848740b4ec8
YogaHadiN <yoga_email@yahoo.com>
2020-08-20 13:57:39 +0700

    dfdfasdfasdfasfasfasfdasdfasfasfasdfsafasfdasfasdfas

--- app/Http/Controllers/WoowaController.php
+++ app/Http/Controllers/WoowaController.php
@@ -8,7 +8,8 @@ use Log;
 
 class WoowaController extends Controller
 {
-	public function webhook(){
+	public function webhookc(){
+		Log::info("pek cun");
 		$json = file_get_contents('php://input');
 		$data = json_decode($json);
 		file_put_contents("listen.txt", print_r($data,1));
