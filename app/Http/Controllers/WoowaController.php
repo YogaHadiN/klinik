3bd1b3861eb6265bd1df5c5962edec41ef865986
YogaHadiN <yoga_email@yahoo.com>
2020-08-21 16:31:23 +0700

    dfdasdfasdfasdfasdfa

--- app/Http/Controllers/WoowaController.php
+++ app/Http/Controllers/WoowaController.php
@@ -10,12 +10,20 @@ class WoowaController extends Controller
 {
 	public function webhook(){
 		$json = file_get_contents('php://input');
+
 		Log::info('this is sparta');
 		Log::info($json);
+
 		$data = json_decode($json);
+
 		Log::info('contact name');
 		Log::info($data->contact_name);
+
 		Log::info('message');
 		Log::info($data->message);
+
+		$message = 'Selamat Siang. Terima kasih telah menghubungi kami. Ada yang dapat kami bantu?';
+
+		Sms::send($data->contact_name, $message);
 	}
 }
