2aab7c7768274656f80f8ca35f05bdaefe7f4976
YogaHadiN <yoga_email@yahoo.com>
2020-08-22 12:20:11 +0700

    tanggal lahir

--- app/Http/Controllers/WoowaController.php
+++ app/Http/Controllers/WoowaController.php
@@ -12,19 +12,23 @@ class WoowaController extends Controller
 	public function webhook(){
 		$json = file_get_contents('php://input');
 
-		Log::info('this is sparta');
 		Log::info($json);
 
 		$data = json_decode($json);
+		$message = $data->message;
+		$no_telp = $data->contact_name;
 
-		Log::info('contact name');
-		Log::info($data->contact_name);
+		if ( $message == 'daftar' ) {
+			$message = 'Bisa dibantu tanggal lahirnya? Contoh 19 Juli 1993 kirim 19-07-1983';
+			Sms::send($no_telp, $message);
 
-		Log::info('message');
-		Log::info($data->message);
+			/* $whatsapp_registration            = new WhatsappRegistration; */
+			/* $whatsapp_registration->no_telp   = $np_telp; */
+			/* $whatsapp_registration->reg_level = 1; */
+			/* $whatsapp_registration->save(); */
 
-		$message = 'Selamat Siang. Terima kasih telah menghubungi kami. Ada yang dapat kami bantu?';
-
+		}
+		/* $message = 'Selamat Siang. Terima kasih telah menghubungi kami. Ada yang dapat kami bantu?'; */
 		/* Sms::send($data->contact_name, $message); */
 	}
 }
