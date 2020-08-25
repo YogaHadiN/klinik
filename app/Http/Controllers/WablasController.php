f579b212cc87c143ed5b7aafd63489cddcf490cd
YogaHadiN <yoga_email@yahoo.com>
2020-08-25 09:20:03 +0700

    djfweoirweorewsddf

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class WablasController extends Controller
 {
 	public function webhook(){
+		Log::info( ' ================================== INFO ===========================' );
+		Log::info($_POST['message']);
+		Log::info( ' ================================== INFO ===========================' );
 		if(isset($_POST['message'])) {
 			Log::info( ' ================================== WABLAS ===========================' );
 			Log::info($_POST['message']);
