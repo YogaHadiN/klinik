6074901f84caf0736b10443911db1e808951cc65
YogaHadiN <yoga_email@yahoo.com>
2020-09-06 14:37:43 +0700

    peringatan

--- resources/views/fasilitas/memproses.blade.php
+++ resources/views/fasilitas/memproses.blade.php
@@ -37,6 +37,28 @@
 								<td>{{ $antrian->whatsapp_registration->nomor_bpjs }}</td>
 							@endif
 						</tr>
+						<tr>
+							<th>Keluhan</th>
+							<td>
+								<ul>
+									@if ( $antrian->whatsapp_registration->demam  )
+										<li>Demam</li>		
+									@endif
+									@if ( $antrian->whatsapp_registration->batuk_pilek  )
+										<li>Batuk Pilek</li>		
+									@endif
+									@if ( $antrian->whatsapp_registration->nyeri_menelan  )
+										<li>Nyeri Menelan</li>		
+									@endif
+									@if ( $antrian->whatsapp_registration->sesak_nafas  )
+										<li>Sesak Nafas</li>		
+									@endif
+									@if ( $antrian->whatsapp_registration->kontak_covid  )
+										<li>Kontak COVID +</li>		
+									@endif
+								</ul>
+							</td>
+						</tr>
 					</tbody>
 				</table>
 			</div>
