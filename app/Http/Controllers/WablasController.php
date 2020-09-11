05aaf14c0b27e725091871353316296c8c6594dc
YogaHadiN <yoga_email@yahoo.com>
2020-09-11 23:19:02 +0700

    simplify things

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -368,20 +368,17 @@ class WablasController extends Controller
 			$text .= 'Balas *A* untuk Dokter Umum, ';
 			$text .= PHP_EOL;
 			$text .= PHP_EOL;
-			$text .= 'Balas *B* untuk Dokter Gigi, ';
-			$text .= PHP_EOL;
-			$text .= '(Pendaftaran Dibuka Senin - Jumat jam 15.00 - 20.00)';
-			$text .= PHP_EOL;
-			$text .= PHP_EOL;
+			if ( $this->gigi_buka ) {
+				$text .= 'Balas *B* untuk Dokter Gigi, ';
+				$text .= PHP_EOL;
+				$text .= PHP_EOL;
+			}
 			$text .= 'Balas *C* untuk Suntik KB/Periksa Hamil.';
-			/* $text .= PHP_EOL; */
-			/* $text .= PHP_EOL; */
-			/* $text .= 'Bila berobat ke Dokter estetik / kecantikan atau USG Kebidanan silahkan daftar langsung'; */
 			$text .= PHP_EOL;
 			$text .= PHP_EOL;
-			$text .= 'Balas *D* untuk Dokter Estetika/Kecantikan';
-			$text .= PHP_EOL;
-			$text .= '(Pendaftaran Dibuka Senin - Jumat jam 11.00 - 17.00)';
+			if ( $this->estetika_buka ) {
+				$text .= 'Balas *D* untuk Dokter Estetika/Kecantikan';
+			}
 			/* $text .= PHP_EOL; */
 			/* $text .= PHP_EOL; */
 			/* $text .= 'Balas *E* untuk USG Kebidanan'; */
